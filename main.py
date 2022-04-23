13fc4dc83c21ee8a1f8e4a2c3dfeb9a96bc5b5a5
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-04-23 21:12:51 +0200

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+from public_tests import *
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+# Example of a picture
+index = 25
+plt.imshow(train_set_x_orig[index])
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[2]
