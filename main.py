48bc4d22e78c6978b69e3f0ff06f335fe4d140d7
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-04-26 23:01:29 +0200

    undo

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[2]
 
-train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1)
-test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1)
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 
 train_set_x = train_set_x_flatten / 255.
 test_set_x = test_set_x_flatten / 255.
